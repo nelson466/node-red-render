171405d892be636a0ea994815515c4fe5b981039
nelson466 <nvcamcho@gmail.com>
2025-03-28 16:55:14 -0400

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Configura aquí la clave de encriptación.
+  // Es recomendable establecer NODE_RED_CREDENTIAL_SECRET como variable de entorno en Render.com.
+  credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET || "miClaveSecreta123"
+  
+  // Puedes agregar más configuraciones de Node‑RED aquí si lo necesitas.
+};
