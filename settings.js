830ba9dc092d812d19f012d5ff6fda4778ffb227
nelson466 <nvcamcho@gmail.com>
2025-05-22 18:14:52 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  // Especifica el nombre del archivo de flujos que quieres usar
-  flowFile: 'flows.json',
-  // Clave que Node-RED usa para cifrar/descifrar tus credenciales de nodo
-  // (asegúrate de tener definida NODE_RED_CREDENTIAL_SECRET en Render)
+  // Configura aqui la clave de encriptacion.
+  // Es recomendable establecer NODE_RED_CREDENTIAL_SECRET como variable de entorno en Render.com
   credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET || "miClaveSecreta123",
-  // Aquí podrías añadir más configuraciones si las necesitas...
+  // Puedes agregar mas configuraciones de Node-RED aqui si lo necesitas...
 };
